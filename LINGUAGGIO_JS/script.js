3b9dce481973ae14219c334ce7e7a77bc44f9fd6
andrea <andreabrighi0902@gmail.com>
2025-11-26 10:13:31 +0100

    commit1

---
+++ LINGUAGGIO_JS/script.js
@@ -0,0 +1,14 @@
+/*commento multilinea */
+//commento in linea
+
+// Stampa in console
+console.log("Ciao, mondo");
+
+// Mostra un pop-up con un messaggio
+window.alert("Benvenuto");
+
+//Mostra un pop-up con una richiesta che ritorna true/false
+window.confirm("Sei sicuro?")
+
+//Mostra un pop-up che chiede un dato
+window.
